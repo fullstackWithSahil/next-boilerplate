6063df4fbbeac21be9c83752b17d30632028e4ba
fullstackwithsahil <fullstackwithsahil@gmail.com>
2024-11-18 09:10:58 +0530

    first commit

---
+++ src/database/index.ts
@@ -0,0 +1,26 @@
+import mongoose, { Mongoose } from "mongoose";
+
+interface MongooseConnection {
+    conn: Mongoose | null;
+    promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+    cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+export async function connectMongo() {
+    if (cached.conn) return cached.conn;
+
+    if (!process.env.MONGODB_URL) {
+        throw new Error("MongoDB connection URL is not provided.");
+    }
+
+    cached.promise = cached.promise || mongoose.connect(process.env.MONGODB_URL!, { dbName: "buisnesstools", bufferCommands: false });
+
+    // Await the connection promise
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
